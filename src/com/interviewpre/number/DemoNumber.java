8aec2beb83fabd02620e06a05a8f84b4e9b4d6c5
Kumarsb22 <kumarsb020@gmail.com>
2025-07-10 18:28:38 +0530

    pdf number programs and String reverse

--- src/com/interviewpre/number/DemoNumber.java
+++ src/com/interviewpre/number/DemoNumber.java
@@ -11,6 +11,47 @@ public class DemoNumber {
 		System.out.println();
 		System.out.println("Sum of the digits is " + sumOfDigit(12345));
 		System.out.println("The Number is armstrong = " + isArmstrongNum(153));
+		System.out.println("The Number is Strong = " + isStrongNumber(145));
+		System.out.println("The Power of the Numbern is " + powerNumber(2, 3));
+		System.out.println("The number is palidrome " + isNumberPalindrome(12321));
+		System.out.println("Reverse the number " + reverseNumber(123));
+		System.out.println("Digit count " + digitCount(12345));
+		System.out.println("Binary count " + binaryCount(123401010));
+		quatRem(12, 4);
+		System.out.println(revStringUFor("I Love Myself"));
+		System.out.println(revStrinUWhileAndisStringPalindrom("gadag"));
+
+	}
+
+	private static String revStringUFor(String s) {
+		StringBuilder builder = new StringBuilder();
+		for (int i = s.length() - 1; i >= 0; i--) {
+			builder.append(s.charAt(i));
+		}
+		return builder.toString();
+	}
+
+	private static boolean revStrinUWhileAndisStringPalindrom(String s) {
+		String s2 = s;
+		int n = s.length() - 1;
+		StringBuilder builder = new StringBuilder();
+		while (n >= 0) {
+			builder.append(s.charAt(n--));
+		}
+		System.out.println("Reversed the String using while loop " + builder.toString());
+		return builder.toString().equals(s2);
+	}
+
+	private static String revStriWiLoops(String s) {
+		char[] charArray = s.toCharArray();
+		int i = 0, j = s.length() - 1;
+
+		return s;
+	}
+
+	private static void quatRem(int a, int b) {
+		System.out.println("Quotient " + (a / b));
+		System.out.println("Remender " + (a % b));
 	}
 
 	private static void evenOrOdd(int num) {
@@ -96,7 +137,63 @@ public class DemoNumber {
 			copy /= 10;
 		}
 		return sum == n;
+	}
 
+	private static boolean isStrongNumber(int n) {
+		int sum = 0, m = n;
+		while (m != 0) {
+			int rem = m % 10;
+			int fact = 1;
+			for (int i = 1; i <= rem; i++)
+				fact *= i;
+			sum += fact;
+			m /= 10;
+		}
+		return n == sum;
+	}
+
+	private static int binaryCount(int n) {
+		int count = 0;
+		while (n != 0) {
+			int rem = n % 10;
+			if (rem == 0 || rem == 1)
+				count++;
+			n /= 10;
+		}
+		return count;
+	}
+
+	private static int digitCount(int n) {
+		int count = 0;
+		while (n != 0) {
+			n = n / 10;
+			count++;
+		}
+		return count;
+	}
+
+	private static int reverseNumber(int n) {
+		int rev = 0;
+		while (n != 0) {
+			int rem = n % 10;
+			rev = rev * 10 + rem;
+			n = n / 10;
+		}
+		return rev;
+	}
+
+	private static boolean isNumberPalindrome(int n) {
+		int rev = 0, m = n;
+		while (m != 0) {
+			int rem = m % 10;
+			rev = rev * 10 + rem;
+			m = m / 10;
+		}
+		return n == rev;
+	}
+
+	private static double powerNumber(int n, int p) {
+		return Math.pow(n, p);
 	}
 
 }
